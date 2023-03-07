48e917cc2d646c1f97f6bad2d47bb89ce8aeea98
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:00:04 -0500

    feat(Add Exercise): :sparkles: .charAt()
    This is the .charAt() method in ES6

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const mensaje = "Hola, ¿cómo estás?";
-console.log(mensaje.length); // salida: 18
+// El paramaetro que recibe el metodo es el indice del arreglo
+console.log(mensaje.charAt(0)); // salida: "H"
+console.log(mensaje.charAt(2)); // salida: "l"
