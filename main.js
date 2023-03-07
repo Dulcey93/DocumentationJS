637a91ff7d3c7d0071088abe171d82aed6f8c91e
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:37:21 -0500

    feat(Add Exercise): :sparkles: .endsWith()
    This is the .endsWith() method in ES6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const mensaje = 'Hola Mundo!';
-const comienzaConHola = mensaje.startsWith('Hola');
-console.log(comienzaConHola); // salida: true
+const terminaConMundo = mensaje.endsWith('Mundo!');
+console.log(terminaConMundo); // salida: true
 
-// En este ejemplo, el método String.startsWith() se utiliza para verificar si una cadena de texto comienza con una subcadena dada. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método startsWith() se utiliza para verificar si la cadena de texto comienza con la subcadena "Hola". El resultado de la verificación se almacena en la variable comienzaConHola, que es un valor booleano (true o false).
+// En este ejemplo, el método String.endsWith() se utiliza para verificar si una cadena de texto termina con una subcadena dada. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método endsWith() se utiliza para verificar si la cadena de texto termina con la subcadena "Mundo!". El resultado de la verificación se almacena en la variable terminaConMundo, que es un valor booleano (true o false).
