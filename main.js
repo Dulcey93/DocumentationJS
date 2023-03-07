3dcf0fb21d4781c16f4c0a2ad2d63feededf3e4b
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:06:26 -0500

    feat(Add Exercise): :sparkles: .charCodeAt()
    This is the .charCodeAt() method in ES6

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const mensaje = "Hola, ¿cómo estás?";
 // El paramaetro que recibe el metodo es el indice del arreglo
-console.log(mensaje.charAt(0)); // salida: "H"
-console.log(mensaje.charAt(2)); // salida: "l"
+// se utiliza para obtener el código ASCII del carácter indicado en el parámetro del método
+console.log(mensaje.charCodeAt(0)); // salida: 72
+console.log(mensaje.charCodeAt(2)); // salida: 108
+
