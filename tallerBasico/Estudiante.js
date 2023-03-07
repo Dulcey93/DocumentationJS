2411f92552f61970dba3779ff4f7eae8c81f6b9d
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 09:01:27 -0500

    feat(First void commit): :sparkles: This is the first void commit without content
    I try to make the first commit without info in the files
    
    New issue we don't have content in the files

--- tallerBasico/Estudiante.js
+++
@@ -1,8 +0,0 @@
-export default class Estudiante{
-    nombre;
-    nota;
-    constructor(nombre, nota){
-        this.nombre = nombre;
-        this.nota = nota;
-    }
-}
--- tallerBasico/main.js
+++
@@ -1,8 +0,0 @@
-import { Estudiante } from "./Estudiante.js";
-import { Estandar } from "./Estandar.js";
-
-const nombre = prompt("Ingrese su nombre");
-
-const nota = prompt("Ingrese su nota");
-const estudiante1 = new Estudiante(nombre, nota);
-const estandar1 = new Estandar();
