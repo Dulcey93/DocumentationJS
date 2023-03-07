a6e8ac8ad852504aa135797fcd5d3b103d74ba9b
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:27:34 -0500

    feat(Add Exercise): :sparkles: .fromCodePoint()
    This is the .fromCodePoint() method in ES6

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// El método String.fromCharCode() no es parte de ES6, sino que es un método heredado de versiones anteriores de JavaScript. Sin embargo, todavía se puede utilizar en la versión actual de JavaScript (ES6).
+const codigo1 = 128077;
+const codigo2 = 127995;
+const codigo3 = 128516;
 
-const codigo1 = 72;
-const codigo2 = 111;
-const codigo3 = 108;
+const emoji = String.fromCodePoint(codigo1, codigo2, codigo3);
+console.log(emoji); // salida: "👍🏼💛😄"
+// En este ejemplo, el método String.fromCodePoint() se utiliza para crear una cadena de texto que representa un emoji compuesto por tres caracteres Unicode diferentes. Los códigos de caracteres Unicode se especifican como argumentos separados por comas para el método.
+
+// El método String.fromCodePoint() también puede tomar una cantidad variable de argumentos y concatenarlos en una cadena de texto. Por ejemplo:
+
 const codigo4 = 97;
+const codigo5 = 98;
+const codigo6 = 99;
+const codigo7 = 128520;
 
-const texto = String.fromCharCode(codigo1, codigo2, codigo3, codigo4);
-console.log(texto); // salida: "Hola"
-
-// En este ejemplo, el método String.fromCharCode() se utiliza para crear una cadena de texto a partir de los códigos de caracteres Unicode de los caracteres "H", "o", "l" y "a". Los códigos de caracteres Unicode se especifican como argumentos separados por comas para el método.
+const texto = String.fromCodePoint(codigo4, codigo5, codigo6, codigo7);
+console.log(texto); // salida: "abc😸"
