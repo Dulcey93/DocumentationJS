cefc413ef8d36634b3a265be6f0448a64d5d14c0
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:42:49 -0500

    feat(Add Exercise): :sparkles: .includes()
    This is the .include() method in ES6, it returns a validation BOOLEAN

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const mensaje = 'Hola Mundo!';
-const terminaConMundo = mensaje.endsWith('Mundo!');
-console.log(terminaConMundo); // salida: true
+const incluyeMundo = mensaje.includes('Mundo');
+console.log(incluyeMundo); // salida: true
 
-// En este ejemplo, el método String.endsWith() se utiliza para verificar si una cadena de texto termina con una subcadena dada. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método endsWith() se utiliza para verificar si la cadena de texto termina con la subcadena "Mundo!". El resultado de la verificación se almacena en la variable terminaConMundo, que es un valor booleano (true o false).
+// En este ejemplo, el método String.includes() se utiliza para verificar si una cadena de texto contiene una subcadena dada. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método includes() se utiliza para verificar si la cadena de texto contiene la subcadena "Mundo". El resultado de la verificación se almacena en la variable incluyeMundo, que es un valor booleano (true o false).
