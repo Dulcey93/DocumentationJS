923ecc8e0104211fbaf338815ac424ece5d9a883
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 18:05:16 -0500

    feat(Add Exercise): :sparkles: .localeCompare()
    .localeCompare() RETURNS A NUMBER TO INDICATE > < or =

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const mensaje = 'Hola Mundo!';
-const indice = mensaje.search('Mundo');
-console.log(indice); // salida: 5
+const mensaje1 = 'árbol';
+const mensaje2 = 'zorro';
+const resultado = mensaje1.localeCompare(mensaje2);
+console.log(resultado); // salida: -23
 
-// En este ejemplo, el método String.search() se utiliza para buscar la primera aparición de una expresión regular en una cadena de texto. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método search() se utiliza para buscar la expresión regular "Mundo" en la cadena de texto. El resultado se almacena en la variable indice.
+// En este ejemplo, el método String.localeCompare() se utiliza para comparar dos cadenas de texto y devolver un número que indica si la cadena actual es mayor, menor o igual a la otra cadena en orden lexicográfico, teniendo en cuenta las reglas de ordenamiento de caracteres para el idioma y la región especificados. Las variables mensaje1 y mensaje2 contienen cadenas de texto que dicen "árbol" y "zorro", respectivamente. Luego, el método localeCompare() se utiliza para comparar las cadenas de texto. El resultado se almacena en la variable resultado.
 
-// En este ejemplo, el resultado impreso en la consola es 5, que es el índice de la primera aparición de la expresión regular "Mundo". Si la expresión regular no hubiera aparecido en la cadena de texto, el resultado habría sido -1. Es importante tener en cuenta que search() solo busca la primera aparición de la expresión regular. Si la expresión regular aparece varias veces en la cadena de texto, search() solo devuelve el índice de la primera aparición.
+// En este ejemplo, el resultado impreso en la consola es -23, lo que indica que la cadena mensaje1 ("árbol") es menor que la cadena mensaje2 ("zorro") en orden lexicográfico para el idioma y la región especificados. Si las cadenas de texto fueran iguales, el resultado habría sido 0. Si la cadena mensaje1 fuera mayor que la cadena mensaje2, el resultado habría sido un número positivo.
