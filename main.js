21e8032a144607585ca16b8cb00ba30e36f96272
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:45:49 -0500

    feat(Add Exercise): :sparkles: .indexOf()
    This method returns the index of the first call to the word we put in the parameter .indexOf()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mensaje = 'Hola Mundo!';
-const incluyeMundo = mensaje.includes('Mundo');
-console.log(incluyeMundo); // salida: true
+const indice = mensaje.indexOf('Mundo');
+console.log(indice); // salida: 5
 
-// En este ejemplo, el método String.includes() se utiliza para verificar si una cadena de texto contiene una subcadena dada. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método includes() se utiliza para verificar si la cadena de texto contiene la subcadena "Mundo". El resultado de la verificación se almacena en la variable incluyeMundo, que es un valor booleano (true o false).
+// En este ejemplo, el método String.indexOf() se utiliza para obtener el índice de la primera aparición de una subcadena dada en una cadena de texto. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método indexOf() se utiliza para obtener el índice de la subcadena "Mundo". El resultado se almacena en la variable indice.
+
