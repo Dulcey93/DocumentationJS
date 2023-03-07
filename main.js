023c21c2420cef13ea6daa8f2bd104d59f90ce67
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:59:02 -0500

    feat(Add Exercise): :sparkles: .search()
    This is the .search method in ES6 RETURN NUMBER

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mensaje = 'Hola Mundo!';
-const ultimoIndice = mensaje.lastIndexOf('o');
-console.log(ultimoIndice); // salida: 9
+const indice = mensaje.search('Mundo');
+console.log(indice); // salida: 5
 
-// En este ejemplo, el método String.lastIndexOf() se utiliza para obtener el índice de la última aparición de un carácter o subcadena dada en una cadena de texto. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método lastIndexOf() se utiliza para obtener el índice de la última aparición del carácter "o". El resultado se almacena en la variable ultimoIndice.
+// En este ejemplo, el método String.search() se utiliza para buscar la primera aparición de una expresión regular en una cadena de texto. La variable mensaje contiene una cadena de texto que dice "Hola Mundo!". Luego, el método search() se utiliza para buscar la expresión regular "Mundo" en la cadena de texto. El resultado se almacena en la variable indice.
 
-// En este ejemplo, el resultado impreso en la consola es 9, que es el índice de la última aparición del carácter "o". Si el carácter no hubiera aparecido en la cadena de texto, el resultado habría sido -1. Es importante tener en cuenta que lastIndexOf() devuelve el índice de la última aparición de la subcadena. Si la subcadena aparece varias veces en la cadena de texto, lastIndexOf() solo devuelve el índice de la última aparición.
+// En este ejemplo, el resultado impreso en la consola es 5, que es el índice de la primera aparición de la expresión regular "Mundo". Si la expresión regular no hubiera aparecido en la cadena de texto, el resultado habría sido -1. Es importante tener en cuenta que search() solo busca la primera aparición de la expresión regular. Si la expresión regular aparece varias veces en la cadena de texto, search() solo devuelve el índice de la primera aparición.
