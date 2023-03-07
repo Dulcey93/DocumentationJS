91b337870a37f46e5af764c48e8b54b15671db47
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 17:10:09 -0500

    feat(Add Exercise): :sparkles: .codePointAt()
    This is the .codePointAt() method in ES6

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const mensaje = "Hola, ¿cómo estás?";
+const emoji = '👍🏼';
 // El paramaetro que recibe el metodo es el indice del arreglo
-// se utiliza para obtener el código ASCII del carácter indicado en el parámetro del método
-console.log(mensaje.charCodeAt(0)); // salida: 72
-console.log(mensaje.charCodeAt(2)); // salida: 108
-
+// Para obtener el valor del punto de código Unicode de un carácter específico de este emoji, puedes usar el método .codePointAt() de la siguiente manera:
+console.log(emoji.codePointAt(0)); // salida: 128077
+// También puedes obtener el valor del punto de código Unicode de cualquier otro carácter del emoji, especificando el índice del carácter como argumento para el método .codePointAt(). Por ejemplo, para obtener el valor del punto de código Unicode del segundo carácter del emoji, puedes hacer lo siguiente:
+console.log(emoji.codePointAt(1)); // salida: 127995
+// En este caso, el método .codePointAt() devuelve el valor del punto de código Unicode del segundo carácter del emoji almacenado en la variable emoji, que es 127995 (el valor del punto de código Unicode correspondiente a la tez de piel clara). Cabe destacar que los índices de los caracteres comienzan en 0.
