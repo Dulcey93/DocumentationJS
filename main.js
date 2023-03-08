54c78a704b0b4b80fb8b630398e83a3a9b79d654
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:41:17 -0500

    feat(Add Exercise): :sparkles: .substring()
    Return a String

--- main.js
+++ main.js
@@ -2,17 +2,18 @@
 
 const cadena = "Hola mundo!";
 
-// Y queremos obtener la subcadena "mundo" que está en medio de la cadena. Podemos hacerlo de la siguiente manera utilizando el método substr():
+// Y queremos obtener la subcadena "mundo" que está en medio de la cadena. Podemos hacerlo de la siguiente manera utilizando el método substring():
 
-const subcadena = cadena.substr(5, 5);
+const subcadena = cadena.substring(5, 10);
 
-// En este caso, el método substr() toma los 5 caracteres que están a partir de la posición 5 de la cadena original, creando una nueva subcadena que se guarda en la variable subcadena. Ahora podemos mostrar esta subcadena en una salida de datos:
+
+// En este caso, el método substring() toma los caracteres que están entre las posiciones 5 y 10 de la cadena original (sin incluir el caracter en la posición 10), creando una nueva subcadena que se guarda en la variable subcadena. Ahora podemos mostrar esta subcadena en una salida de datos:
 
 console.log(subcadena); // "mundo"
 
-// Como puedes ver, el método substr() también nos ha permitido obtener una parte de la cadena original. También es posible utilizar valores negativos para contar desde el final de la cadena:
+// Como puedes ver, el método substring() nos ha permitido obtener una parte de la cadena original. También es posible utilizar valores negativos para contar desde el final de la cadena:
 
-const subcadena1 = cadena.substr(-6, 5);
-console.log(subcadena1); // "mundo"
+const subcadena1 = cadena.substring(5, -2);
+console.log(subcadena1); // "Hola"
 
-// En este caso, utilizamos valores negativos para contar desde el final de la cadena, y obtenemos la misma subcadena que en el ejemplo anterior.
+// En este caso, el método substring() interpreta el valor negativo como si fuera 0, por lo que toma los caracteres desde el inicio de la cadena hasta la posición -2 (sin incluir el caracter en la posición -2). De esta manera, obtenemos la subcadena "Hola".
