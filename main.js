88ce16af7e3b82d90d401761451629c3977e6246
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:46:56 -0500

    feat(Add Exercise): :sparkles: .toUpperCase()
    Return a String

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const originalString = "HELLO WORLD";
-const lowerCaseString = originalString.toLowerCase();
+const originalString = "hello world";
+const upperCaseString = originalString.toUpperCase();
 
-console.log(lowerCaseString); // output: "hello world"
+console.log(upperCaseString); // output: "HELLO WORLD"
 
-// En este ejemplo, el método .toLowerCase() convierte todas las letras del string originalString en minúsculas y lo guarda en la variable lowerCaseString. Luego, la función console.log() imprime lowerCaseString en la consola, lo que mostrará el string convertido a minúsculas.
+// En este ejemplo, el método .toUpperCase() convierte todas las letras del string originalString en mayúsculas y lo guarda en la variable upperCaseString. Luego, la función console.log() imprime upperCaseString en la consola, lo que mostrará el string convertido a mayúsculas.
