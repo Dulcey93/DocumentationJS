ef47c4a59344137b45d3707c0bd485f118ec6a28
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:14:25 -0500

    feat(Add Exercise): :sparkles: .padStart()
    This is the .padStart() method in ES6

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// El método padEnd() se utiliza para agregar caracteres al final de una cadena hasta que la cadena tenga una longitud determinada. Esto puede ser útil para formatear cadenas o para alinearlas correctamente en una salida de datos.
+// El método padStart() se utiliza para agregar caracteres al inicio de una cadena hasta que la cadena tenga una longitud determinada. Esto puede ser útil para formatear cadenas o para alinearlas correctamente en una salida de datos.
 
-const nombre = "Juan";
-const nombreFormateado = nombre.padEnd(20, " ");
+const numero = "23";
+const numeroFormateado = numero.padStart(5, "0");
 
-console.log(nombre); // "Juan"
-console.log(nombreFormateado); // "Juan                "
+console.log(numero); // "23"
+console.log(numeroFormateado); // "00023"
 
-// la cadena nombre tiene una longitud de 4 caracteres, mientras que la cadena nombreFormateado tiene una longitud de 20 caracteres después de haber sido formateada con espacios en blanco agregados al final.
+
+// la cadena numero tiene una longitud de 2 caracteres, mientras que la cadena numeroFormateado tiene una longitud de 5 caracteres después de haber sido formateada con ceros agregados al inicio.
