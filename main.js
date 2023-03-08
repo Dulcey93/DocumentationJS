036d4b55df5f1e91dc6c81a85403546cb5966a16
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:26:33 -0500

    feat(Add Exercise): :sparkles: .replace()
    Returns a String

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-// El método repeat() se utiliza para repetir una cadena un número determinado de veces. Esto puede ser útil para crear cadenas de caracteres repetidas o para formatear una salida de datos.
+// Supongamos que tenemos la siguiente cadena:
 
-const asteriscos = "*".repeat(5);
+const cadena = "Hola mundo!";
 
-console.log(asteriscos); // "*****"
+// Y queremos reemplazar la palabra "mundo" por "amigos". Podemos hacerlo de la siguiente manera utilizando el método replace():
 
+const nuevaCadena = cadena.replace("mundo", "amigos");
 
+// En este caso, el método replace() busca la palabra "mundo" en la cadena original y la reemplaza por la palabra "amigos", creando una nueva cadena que se guarda en la variable nuevaCadena. Ahora podemos mostrar esta cadena en una salida de datos:
 
-// la cadena "*" se repite 5 veces utilizando el método repeat() y se muestra en la consola como una cadena de 5 asteriscos.
+console.log(nuevaCadena); // "Hola amigos!"
+
+// la palabra "mundo" ha sido reemplazada por la palabra "amigos" utilizando el método replace().
+
+// Además, es posible utilizar expresiones regulares con el método replace() para hacer reemplazos más complejos. Por ejemplo, podemos utilizar una expresión regular para reemplazar todas las ocurrencias de una palabra en una cadena:
+
+const cadena1 = "La casa es roja y la puerta es roja también.";
+const nuevaCadena1 = cadena1.replace(/roja/g, "verde");
+console.log(nuevaCadena1); // "La casa es verde y la puerta es verde también."
+
+// En este caso, utilizamos la expresión regular /roja/g para buscar todas las ocurrencias de la palabra "roja" en la cadena original y reemplazarlas por la palabra "verde". El modificador "g" indica que la búsqueda se hará globalmente en toda la cadena.
