e87299d259e8f39ea2354f6bdde6f5756fb8250a
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-08 12:51:30 -0500

    feat(Operadores ES6): :rocket: Operadores comparativos en JS
    This is a brief description about how compare in Js

--- main.js
+++ main.js
@@ -1,78 +1,27 @@
-const euler = Math.E
-const logn2 = Math.LN2
-const logn10 = Math.LN10
-const log2E = Math.LOG2E
-const logn10E = Math.LOG10E
-const PI = Math.PI
-const raiz = Math.SQRT1_2
-const raiz2 = Math.SQRT2
+// Los operadores de comparación en JavaScript se utilizan para comparar dos valores y devolver un valor booleano (verdadero o falso) que indica si la comparación es verdadera o falsa. Los siguientes son algunos de los operadores de comparación en JavaScript:
 
-console.log(euler);
-console.log(logn2);
-console.log(logn10);
-console.log(log2E);
-console.log(logn10E);
-console.log(PI);
-console.log(raiz);
-console.log(raiz2);
+//     ==: Compara si dos valores son iguales (no estrictamente iguales).
+//     ===: Compara si dos valores son iguales y del mismo tipo de datos (estrictamente iguales).
+//     !=: Compara si dos valores no son iguales (no estrictamente diferentes).
+//     !==: Compara si dos valores no son iguales y del mismo tipo de datos (estrictamente diferentes).
+//     <: Compara si el valor de la izquierda es menor que el valor de la derecha.
+//     >: Compara si el valor de la izquierda es mayor que el valor de la derecha.
+//     <=: Compara si el valor de la izquierda es menor o igual que el valor de la derecha.
+//     >=: Compara si el valor de la izquierda es mayor o igual que el valor de la derecha.
 
-/* ===> METODOS MATEMATICOS <=== */
+// A continuación se presentan algunos ejemplos de uso de estos operadores:
 
-Math.abs(-5);             // 5
-Math.sign(-5);            // -1
-Math.exp(1);              // e, o sea, 2.718281828459045
-Math.expm1(1);            // 1.718281828459045
-Math.max(1, 40, 5, 15);   // 40
-Math.min(5, 10, -2, 0);   // -2
-Math.pow(2, 10);          // 1024 (Equivale a 2**10)
-Math.sqrt(2);             // 1.4142135623730951 (Equivale a Math.SQRT2)
-Math.cbrt(2);             // 1.2599210498948732
-Math.imul(0xffffffff, 7); // -7
+let a = 5;
+let b = 10;
+let c = "5";
 
-// Ejemplo de clz32 (count leading zeros)
-const x = 1;
-"0".repeat(Math.clz32(x)) + x.toString(2);
-// Devuelve "00000000000000000000000000000001"
+console.log(a == b);    // false
+console.log(a === c);   // false
+console.log(a != b);    // true
+console.log(a !== c);   // true
+console.log(a < b);     // true
+console.log(a > b);     // false
+console.log(a <= b);    // true
+console.log(a >= b);    // false
 
-
-
-/* ===> RANDOM <===*/
-
-// Obtenemos un número al azar entre [0, 1) con 16 decimales
-let y = Math.random();
-
-// Multiplicamos dicho número por el valor máximo que buscamos (5)
-y = y * 5;
-
-// Redondeamos inferiormente, quedándonos sólo con la parte entera
-y = Math.floor(x);
-
-/* ===> Métodos de logaritmos <===*/
-
-Math.log(2);    // 0.6931471805599453
-Math.log10(2);  // 0.3010299956639812
-Math.log2(2);   // 1
-Math.log1p(2);  // 1.0986122886681096
-
-/* ===> Métodos de redondeo  <===*/
-
-// Redondeo natural, el más cercano
-Math.round(3.75);           // 4
-Math.round(3.25);           // 3
-
-// Redondeo superior (el más alto)
-Math.ceil(3.75);            // 4
-Math.ceil(3.25);            // 4
-
-// Redondeo inferior (el más bajo)
-Math.floor(3.75);           // 3
-Math.floor(3.25);           // 3
-
-// Redondeo con precisión
-Math.round(3.123456789);    // 3
-Math.fround(3.123456789);   // 3.1234567165374756
-
-// Truncado (sólo parte entera)
-Math.trunc(3.75);           // 3
-Math.round(-3.75);          // -4
-Math.trunc(-3.75);          // -3
+// En este ejemplo, se define tres variables: a, b y c. Luego, se realizan algunas operaciones de comparación utilizando los operadores mencionados anteriormente. Por ejemplo, la primera operación a == b devuelve false porque a es igual a 5 y b es igual a 10. También se realiza la operación a === c que devuelve false porque aunque a y c tienen el mismo valor, c es un string y a es un número.
