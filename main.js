4e20d481752875c3dde354d7da58c6764df239dc
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:53:54 -0500

    feat(Add Exercise): :sparkles: .trimStart()
    Return a String without a Space in the Start of the String

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const originalString = "   Hello world!   ";
-const trimmedEndString = originalString.trimEnd();
+const trimmedStartString = originalString.trimStart();
 
-console.log(trimmedEndString); // output: "   Hello world!"
+console.log(trimmedStartString); // output: "Hello world!   "
 
-// En este ejemplo, el método .trimEnd() elimina todos los espacios en blanco al final del string originalString y lo guarda en la variable trimmedEndString. Luego, la función console.log() imprime trimmedEndString en la consola, lo que mostrará el string sin los espacios en blanco al final.
+// En este ejemplo, el método .trimStart() elimina todos los espacios en blanco al inicio del string originalString y lo guarda en la variable trimmedStartString. Luego, la función console.log() imprime trimmedStartString en la consola, lo que mostrará el string sin los espacios en blanco al inicio.
