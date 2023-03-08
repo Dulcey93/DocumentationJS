f881dda37111c88d948a07d85bf8f18056320dc7
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:07:38 -0500

    feat(Add Exercise): :sparkles: .normalize()
    This is the .normalize() method in ES6

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const mensaje = 'Este es un ejemplo de un mensaje con varias palabras repetidas, como esta palabra: ejemplo. También aparece la palabra repetidas varias veces';
-const expresionRegular = /(\w+)/g;
-const coincidencias = mensaje.matchAll(expresionRegular);
+// El método normalize() se utiliza para normalizar una cadena de caracteres, es decir, para convertir caracteres con diferentes formas de escritura (como acentos, diéresis, entre otros) a una forma común. Esto puede ser útil para comparar cadenas o para garantizar que se estén utilizando los caracteres correctos.
 
-for (const coincidencia of coincidencias) {
-    console.log(`Coincidencia encontrada: "${coincidencia[0]}" en el índice ${coincidencia.index}`);
-    // console.log(coincidencia);
-}
+// Supongamos que tenemos el siguiente texto con una letra "á" escrita de dos maneras diferentes:
 
-// En este ejemplo, se utiliza el método matchAll() para buscar todas las palabras en una cadena de texto. La expresión regular utiliza el modificador g para buscar todas las coincidencias, y utiliza el conjunto de caracteres \w para encontrar cualquier palabra (una secuencia de caracteres alfanuméricos y guiones bajos). El método matchAll() devuelve un iterador que puede ser recorrido con un ciclo for...of.
+const texto = "Este texto tiene una letra á escrita de dos formas diferentes: á y á";
 
-// En el ciclo for...of, se itera sobre cada coincidencia encontrada, que es un objeto que contiene información sobre la coincidencia, incluyendo la cadena de texto que coincide con la expresión regular y el índice en la cadena de texto donde se encuentra la coincidencia. En este ejemplo, se imprime en la consola cada coincidencia encontrada, junto con su índice en la cadena de texto.
+// Podemos utilizar el método normalize() para convertir ambas formas de escritura en una forma común utilizando el parámetro "NFD" que indica la normalización utilizando la forma canónica de descomposición de caracteres:
+
+const textoNormalizado = texto.normalize("NFD");
+
+// En este caso, la cadena texto se normaliza y se guarda en la variable textoNormalizado. Ahora podemos utilizar este texto normalizado para compararlo con otras cadenas o para realizar otras operaciones.
+
+console.log(texto); // "Este texto tiene una letra á escrita de dos formas diferentes: á y á"
+console.log(textoNormalizado); // "Este texto tiene una letra á escrita de dos formas diferentes: á y á"
+
+// la cadena texto contiene la letra "á" escrita de dos formas diferentes, mientras que la cadena textoNormalizado contiene ambas letras escritas de la misma forma ("á") después de haber sido normalizadas.
