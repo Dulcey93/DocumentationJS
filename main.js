a0a702b6b1b87a540b959ec5cc67cf86b560e86d
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:12:58 -0500

    feat(Add Exercise): :sparkles: .padEnd()
    This is the .padEnd() method in ES6

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-// El método normalize() se utiliza para normalizar una cadena de caracteres, es decir, para convertir caracteres con diferentes formas de escritura (como acentos, diéresis, entre otros) a una forma común. Esto puede ser útil para comparar cadenas o para garantizar que se estén utilizando los caracteres correctos.
+// El método padEnd() se utiliza para agregar caracteres al final de una cadena hasta que la cadena tenga una longitud determinada. Esto puede ser útil para formatear cadenas o para alinearlas correctamente en una salida de datos.
 
-// Supongamos que tenemos el siguiente texto con una letra "á" escrita de dos maneras diferentes:
+const nombre = "Juan";
+const nombreFormateado = nombre.padEnd(20, " ");
 
-const texto = "Este texto tiene una letra á escrita de dos formas diferentes: á y á";
+console.log(nombre); // "Juan"
+console.log(nombreFormateado); // "Juan                "
 
-// Podemos utilizar el método normalize() para convertir ambas formas de escritura en una forma común utilizando el parámetro "NFD" que indica la normalización utilizando la forma canónica de descomposición de caracteres:
-
-const textoNormalizado = texto.normalize("NFD");
-
-// En este caso, la cadena texto se normaliza y se guarda en la variable textoNormalizado. Ahora podemos utilizar este texto normalizado para compararlo con otras cadenas o para realizar otras operaciones.
-
-console.log(texto); // "Este texto tiene una letra á escrita de dos formas diferentes: á y á"
-console.log(textoNormalizado); // "Este texto tiene una letra á escrita de dos formas diferentes: á y á"
-
-// la cadena texto contiene la letra "á" escrita de dos formas diferentes, mientras que la cadena textoNormalizado contiene ambas letras escritas de la misma forma ("á") después de haber sido normalizadas.
+// la cadena nombre tiene una longitud de 4 caracteres, mientras que la cadena nombreFormateado tiene una longitud de 20 caracteres después de haber sido formateada con espacios en blanco agregados al final.
