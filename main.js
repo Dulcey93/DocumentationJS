97c532f3041de948c6d6608d5273401808ec5d59
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:46:15 -0500

    feat(Add Exercise): :sparkles: .toLowerCase()
    Return a String

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-// Supongamos que tenemos la siguiente cadena:
+const originalString = "HELLO WORLD";
+const lowerCaseString = originalString.toLowerCase();
 
-const cadena = "Hola, ¿cómo estás?, espero que bien.";
+console.log(lowerCaseString); // output: "hello world"
 
-// Y queremos dividir esta cadena en un arreglo de subcadenas, utilizando como separador la coma seguida de un espacio (, ). Podemos hacerlo de la siguiente manera utilizando el método split():
-
-const arreglo = cadena.split(", ");
-
-
-// En este caso, el método split() divide la cadena original en varias subcadenas, cada vez que encuentra el separador , en la cadena original. Las subcadenas resultantes se guardan en un arreglo que se asigna a la variable arreglo. Ahora podemos mostrar este arreglo en una salida de datos:
-
-console.log(arreglo); // ["Hola", "¿cómo estás?", "espero que bien."]
-
-// Como puedes ver, el método split() nos ha permitido dividir la cadena original en varias subcadenas, utilizando como separador la coma seguida de un espacio. También es posible utilizar otros separadores, como por ejemplo un espacio simple ( ), una letra, un número, etc.
-
-const arreglo1 = cadena.split(" "); // ["Hola,", "¿cómo", "estás?,", "espero", "que", "bien."]
-const arreglo2 = cadena.split("?"); // ["Hola, ¿cómo estás,", " espero que bien."]
-
-// En el primer ejemplo, hemos utilizado un espacio simple como separador, dividiendo la cadena en cada lugar donde hay un espacio en la cadena original. En el segundo ejemplo, hemos utilizado el signo de interrogación como separador, dividiendo la cadena en cada lugar donde hay un signo de interrogación en la cadena original.
+// En este ejemplo, el método .toLowerCase() convierte todas las letras del string originalString en minúsculas y lo guarda en la variable lowerCaseString. Luego, la función console.log() imprime lowerCaseString en la consola, lo que mostrará el string convertido a minúsculas.
