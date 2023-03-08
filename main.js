d062c818fbdf00276a667d72048b2bc30f65591a
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-07 19:56:54 -0500

    feat(Add Exercise): :sparkles: .raw` hola ${variable}`
    Return a Static String with variables

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const originalString = "   Hello world!   ";
-const trimmedStartString = originalString.trimStart();
+// El método String.raw() en ES6 se utiliza para crear una cadena de caracteres en bruto (raw string) que contiene secuencias de escape de caracteres literales tal como están escritos. Aquí te muestro un ejemplo:
 
-console.log(trimmedStartString); // output: "Hello world!   "
+const name = "John";
+const age = 25;
 
-// En este ejemplo, el método .trimStart() elimina todos los espacios en blanco al inicio del string originalString y lo guarda en la variable trimmedStartString. Luego, la función console.log() imprime trimmedStartString en la consola, lo que mostrará el string sin los espacios en blanco al inicio.
+const message = String.raw`Hello, my name is ${name} and I am ${age} years old.`;
+
+console.log(message); // output: "Hello, my name is John and I am 25 years old."
+
+// En este ejemplo, la plantilla de cadena de caracteres se define usando el método String.raw(), que toma como argumento un array de literales de plantilla y los valores interpolados. En la plantilla, los valores interpolados se insertan en los espacios reservados ${}. La cadena de caracteres en bruto devuelta por el método String.raw() conserva todas las secuencias de escape de caracteres literales en su forma original.
+
+// El resultado es la cadena de caracteres "Hello, my name is John and I am 25 years old." que se guarda en la variable message y se imprime en la consola mediante console.log().
