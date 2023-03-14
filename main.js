35049c853c1f162cd29b12e23fbc50882a24c3f6
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-14 13:31:00 -0500

    feat(Function): :sparkles: This

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-/* const func = function () {
-    return "Función tradicional.";
-  };
-  
-  const func = () => {
-    return "Función flecha.";
-  };
+// Si son funciones globales
+const a = function () {
+  console.log(this);
+};
+const b = () => {
+  console.log(this);
+};
 
-  const func = function () {
-    return "Función tradicional.";
-  };
-  
-  const func = () => {
-    return "Función flecha.";
-  }; */
+a(); // Window
+b(); // Window
+
+/* Sin embargo, si utilizamos una función en el interior de un objeto, como suele ser el caso más habitual, si encontraremos diferencias. Observa que en la primera función, donde se utiliza una función tradicional, el this devuelve el objeto padre de la función.
+
+Por otro lado, en la segunda función, donde se utiliza una función flecha, el this no devuelve el objeto padre de la función, sino que devuelve Window. */
+
+padre = {
+  a: function () {
+    console.log(this);
+  },
+  b: () => {
+    console.log(this);
+  },
+};
+
+padre.a(); // padre
+padre.b(); // Window
