347be0f6333519590e2e3f59315ad2a25ab7c297
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-14 13:13:37 -0500

    feat(Function): :sparkles: closures

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-// Función autoejecutable
-(function () {
-    console.log("Hola!!");
-  })();
+// Clausura: Función incr()
+function cont (num) {
+
+    return function () {
+      num++;
+      return num;
+    };
+  }
+
+
+  const incr = cont(5)
   
-  // Función autoejecutable con parámetros
-  (function (name) {
-    console.log(`¡Hola, ${name}!`);
-  })("Manz");
-/*   Ten en cuenta, que si la función autoejecutable devuelve algún valor con return, a diferencia de las funciones por expresión, en este caso lo que se almacena en la variable es el valor que devuelve la función autoejecutada: */
-  const f = (function (name) {
-    return `¡Hola, ${name}!`;
-  })("Manz");
-  
-  f; // '¡Hola, Manz!`
-  typeof f; // 'string'
+  typeof incr; // 'function'
+  incr(); // 1
+  incr(); // 2
+  incr(); // 3
+  console.log(incr())
+  console.log(incr())
