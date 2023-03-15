4fac4a663b59235b17a7e1f051d40dcb16a3445f
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-03-14 19:26:04 -0500

    feat(Object.entries(obj)): :construction: Explain the Object.entries(obj) and the Object.fromEntries(matrix)

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-// Si son funciones globales
-const a = function () {
-  console.log(this);
-};
-const b = () => {
-  console.log(this);
-};
+// ¡Claro que sí!
 
-a(); // Window
-b(); // Window
+// Object.fromEntries es un método estático de JavaScript que toma una lista de pares clave-valor (en forma de matriz) y los convierte en un objeto. Cada elemento de la matriz debe ser una matriz de dos elementos, donde el primer elemento es la clave y el segundo es el valor. El método devuelve un nuevo objeto que contiene las claves y valores especificados.
 
-/* Sin embargo, si utilizamos una función en el interior de un objeto, como suele ser el caso más habitual, si encontraremos diferencias. Observa que en la primera función, donde se utiliza una función tradicional, el this devuelve el objeto padre de la función.
+// Por otro lado, new FormData() es una interfaz de JavaScript que nos permite crear objetos que representan datos de formulario HTML. Esto se utiliza comúnmente en aplicaciones web para enviar datos a un servidor mediante solicitudes HTTP.
 
-Por otro lado, en la segunda función, donde se utiliza una función flecha, el this no devuelve el objeto padre de la función, sino que devuelve Window. */
+// Cuando combinamos Object.fromEntries y new FormData(), podemos crear un objeto FormData a partir de un objeto JavaScript. Podemos hacer esto convirtiendo el objeto en una matriz de pares clave-valor, usando el método Object.entries(), y luego pasando esta matriz a Object.fromEntries(). Esto crea un objeto con las mismas claves y valores que el objeto original, pero en un formato que puede ser utilizado por el constructor de FormData.
 
-padre = {
-  a: function () {
-    console.log(this);
-  },
-  b: () => {
-    console.log(this);
-  },
-};
+// ====> Un ejemplo de cómo usarlos juntos sería:<====
 
-padre.a(); // padre
-padre.b(); // Window
+const obj1 = { name: 'John', age: 30 };
+const formData = new FormData(Object.fromEntries(Object.entries(obj1)));
+
+// En este ejemplo, creamos un objeto obj que tiene dos propiedades: name y age. Luego, convertimos este objeto en una matriz de pares clave-valor utilizando Object.entries(), y finalmente utilizamos Object.fromEntries() para convertir esa matriz en un objeto. Por último, creamos un objeto FormData a partir del objeto que acabamos de crear.
+
+// En resumen, la combinación de Object.fromEntries() y new FormData() nos permite crear objetos FormData a partir de objetos JavaScript de manera sencilla.
+
+/* =================>PROCESO DE CONVERSIÓN<========================== */
+
+/* El proceso de conversión de Object.entries() a Object.fromEntries() se realiza en dos pasos: */
+
+/* Object.entries(): Primero, se utiliza el método Object.entries() para convertir el objeto en una matriz de pares clave-valor. Este método devuelve una matriz de matrices, donde cada matriz interna representa un par clave-valor del objeto.
+
+Ejemplo: */
+
+const obj = { name: 'John', age: 30 };
+const entries = Object.entries(obj);
+console.log(entries); // [["name", "John"], ["age", 30]]
+
+/* Object.fromEntries(): A continuación, se utiliza el método Object.fromEntries() para convertir la matriz de pares clave-valor en un objeto. Este método devuelve un objeto a partir de una matriz de pares clave-valor.
+
+Ejemplo: */
+
+const entriesmatrix = [["name", "John"], ["age", 30]];
+const obj2 = Object.fromEntries(entriesmatrix);
+console.log(obj2); // { name: 'John', age: 30 }
+
+// Por lo tanto, la combinación de estos dos métodos permite convertir un objeto en una instancia de FormData en JavaScript.
+
+// BÁSICAMENTE: Object.entries(obj) convierte el objeto en una matriz de matrices con clave-valor
+//              Object.fromEntries(entriesmatrix) convierte la matriz en un objeto
