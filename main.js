72aa54c8d0e35c50f23c5d12039c7647e0442e72
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-04-19 06:50:27 -0500

    feat(Promises): :sparkles: Promises Example

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-/* Ejemplo de Callbacks reutilizables */
-/* Supongamos que queremos crear una función que tome una lista de números y una función de transformación como argumentos, y devuelva una nueva lista que contenga los números transformados. En este ejemplo, utilizaremos callbacks reutilizables para permitir que los usuarios pasen cualquier función de transformación que deseen: */
-const transformarLista = (lista, transformacion) => {
-    const listaTransformada = [];
-    lista.forEach(numero => {
-        listaTransformada.push(transformacion(numero));
+const obtenerNombresUsuarios = (callback) => {
+    return new Promise((resolve, reject) => {
+        // Hacemos una consulta a la base de datos para obtener los nombres de usuarios
+        const usuarios = obtenerUsuariosDeBaseDeDatos();
+
+        // Verificamos si existen los datos, utilizando un operador ternario
+        const nombresUsuarios = usuarios ? usuarios.map(usuario => usuario.nombre) : null;
+
+        // Si los datos son válidos, resolvemos la promesa y ejecutamos la función callback con los nombres de usuarios
+        if (nombresUsuarios) {
+            resolve(nombresUsuarios);
+            callback(null, nombresUsuarios);
+        } else {
+            // Si los datos no son válidos, rechazamos la promesa y ejecutamos la función callback con un mensaje de error
+            const error = 'No se pudieron obtener los nombres de usuarios';
+            reject(error);
+            callback(error, null);
+        }
     });
-    return listaTransformada;
 }
 
-const duplicarNumero = numero => numero * 2;
-const elevarAlCuadrado = numero => numero ** 2;
-
-const numeros = [1, 2, 3, 4, 5];
-
-const numerosDuplicados = transformarLista(numeros, duplicarNumero);
-console.log('Números duplicados:', numerosDuplicados);
-
-const numerosAlCuadrado = transformarLista(numeros, elevarAlCuadrado);
-console.log('Números al cuadrado:', numerosAlCuadrado);
-
-
-/* En este ejemplo, la función transformarLista toma una lista de números y una función de transformación como argumentos, y devuelve una nueva lista que contiene los números transformados. La función duplicarNumero y la función elevarAlCuadrado son dos funciones de transformación que se pueden pasar a la función transformarLista.
-
-Luego, creamos una lista de números y llamamos a la función transformarLista dos veces, pasando la lista de números y una función de transformación diferente en cada llamada. */
+// Llamamos a la función para obtener los nombres de usuarios y manejamos los resultados utilizando la función callback
+obtenerNombresUsuarios((error, nombresUsuarios) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(nombresUsuarios);
+    }
+});
